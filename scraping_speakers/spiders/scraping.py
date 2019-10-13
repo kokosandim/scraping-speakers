9af1a869dc82b78bbde0b0c627daad517017e76b
Prokopenko Nikolay <nikolayjob18121990@gmail.com>
2019-10-13 16:05:13 +0300

    Update scraping.py

--- scraping_speakers/spiders/scraping.py
+++ scraping_speakers/spiders/scraping.py
@@ -3,18 +3,18 @@ import scrapy
 
 
 class ToScrapeSpiderXPath(scrapy.Spider):
-    name = 'scraping-speakers'
+    name = 'scrapSites'
     start_urls = [
         'https://www.tech-festival.com/2019-speakers',
     ]
 
     def parse(self, response):
-        for quote in response.xpath('//div[@class="m-speakers-list__items__item__header"]'):
+        for quote in response.css('div.m-speakers-list__items__item__header'):
             yield {
 #                'link': quote.xpath('./a[@class="full-link"]/@href').extract(),
-                'author': quote.xpath('./h3[@class="m-speakers-list__items__item__header__title"]/a[@class="m-speakers-list__items__item__header__title__link js-librarylink-entry"]/text()').extract_first(),
-                'position': quote.xpath('.//span[@class="m-speakers-list__items__item__header__meta__position"]/text()').extract_first(),
-                'company': quote.xpath('.//span[@class="m-speakers-list__items__item__header__meta__company"]/text()').extract_first()
+'author': quote.css('h3.m-speakers-list__items__item__header__title > a.m-speakers-list__items__item__header__title__link js-librarylink-entry::text').extract_first(),
+'position': quote.css('span.m-speakers-list__items__item__header__meta__position::text').extract_first(),
+'company': quote.css('span.m-speakers-list__items__item__header__meta__company::text').extract_first()
             }
 # Go to next page            
 #        next_page_url = response.css("li.next > a::attr(href)").extract_first()
